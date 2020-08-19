d3da24cd5cf332a5236a50e64f5ebb7eae0bac64
AlejandroCarballo <o.alejandro.carballo@gmail.com>
2020-08-19 15:02:32 -0300

    play with var keyword

---
+++ 011_var_keyword/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var y = 43
+var z int
+
+func main() {
+	x := 42
+	fmt.Println(x)
+
+	fmt.Println(y)
+
+	foo()
+
+	fmt.Println(z)
+}
+func foo() {
+	fmt.Println("againg", y)
+}
