3bd4a80d462f9eb75b0bdb42159a8172b36da464
AlejandroCarballo <o.alejandro.carballo@gmail.com>
2020-08-19 15:05:30 -0300

    Added comments

--- 011_var_keyword/main.go
+++ 011_var_keyword/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+// DECLARE the variable "y"
+// ASSIGN the value 43
+// declare & assign = initilization
 var y = 43
+
+// DECLARE there is a VARIABLE with the IDENTIFIER "z"
+// and that the VARIABLE with the IDENTIFIER "z" is of TYPE int
+// ASSIGNS the ZERO VALUE of TYPE int to "z"
+// false for booleans, 0 for integers, 0.0 for floats, "" for strings,
+// and nil for pointers, functions, interfaces, slices, channels, and maps.
 var z int
 
 func main() {
